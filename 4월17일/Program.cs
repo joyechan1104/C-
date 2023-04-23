17c77a84438a3465df3e4d3c98b9134dab4c57f3
조예찬 <127711957+joyechan1104@users.noreply.github.com>
2023-04-23 14:55:13 +0900

    Add files via upload

---
+++ 4월17일/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("랜덤하게 생성할 수의 개수 : ");
+        int N = int.Parse(Console.ReadLine());
+        Random rand = new Random();
+        int[] arr = new int[N];
+
+        Console.Write("랜덤값출력: ");
+        for (int i = 0; i < N; i++)
+        {
+            arr[i] = rand.Next(0, 100);
+            Console.Write(arr[i] + " ");
+        }
+        Console.WriteLine();
+
+        int max = arr[0], min = arr[0];
+        for (int i = 1; i < N; i++)
+        {
+            if (max < arr[i])
+            {
+                max = arr[i];
+            }
+            if (min > arr[i])
+            {
+                min = arr[i];
+            }
+        }
+        Console.WriteLine("최대값: " + max + " 최소값: " + min);
+    }
+}
