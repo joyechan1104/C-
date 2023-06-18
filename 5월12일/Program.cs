f9bd02ad1a3606b6cabe51c4a7f2204ec1068bac
조예찬 <127711957+joyechan1104@users.noreply.github.com>
2023-06-18 23:08:47 +0900

    너비구하기

---
+++ 5월12일/Program.cs
@@ -0,0 +1,47 @@
+﻿class program
+{
+    class Box
+    {
+        //변수의 속성
+        private int width;
+        public int Width
+        {
+            get { return width; }
+            set
+            {
+                if (value > 0) { width = value; }
+                else { Console.WriteLine("너비는 자연수를 입력해주세요"); }
+            }
+        }
+
+        private int height;
+        public int Height
+        {
+            get { return height; }
+            set
+            {
+                if (value > 0) { height = value; }
+                else { Console.WriteLine("높이는 자연수를 입력해주세요"); }
+            }
+        }
+
+        //생성자
+        public Box(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        //인스턴트 메소드
+        public int Area() { return this.width * this.height; }
+    }
+
+    static void Main(string[] args)
+    {
+        Box box = new Box(+10, -20);
+
+        box.Width = -200;
+        box.Height = +100;
+        Console.WriteLine(box.Area());
+    }
+}
